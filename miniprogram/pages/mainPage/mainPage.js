fbb327ae2c3c79c82dd18576076d2ca337c8b4ab
Ziyan Jerry Chen <jerryc05@qq.com>
2019-10-11 15:50:13 -0400

    integrate swiper to mainPage;
    Signed-off-by: Ziyan "Jerry" Chen <jerryc05@qq.com>

--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -5,14 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    //image slider
+    movies: [
+      { url: '/images/cssa_logo.png' },
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showModal({
+      title: 'Hello',
+      content: 'Welcome to UMD CSSA WeApp Page 2!'
+    })
   },
 
   /**
