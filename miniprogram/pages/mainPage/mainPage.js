c06080f4378789e403e1375bd9f759ba38d26d9c
Ziyan Jerry Chen <jerryc05@qq.com>
2019-10-13 04:31:29 -0400

    update img swiper; add canvas for cssa logo;
    Signed-off-by: Ziyan "Jerry" Chen <jerryc05@qq.com>

--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -2,80 +2,73 @@
 Page({
 
   /**
-   * 页面的初始数据
+   * Page data.
    */
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: '',
-    //image slider
-    movies: [
-      { url: '/images/cssa_logo.png' },
-      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
-      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
-      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
-      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    // [swiper] image slider
+    img_slider_urls: [
+      'https://images.unsplash.com/photo-1558637845-c8b7ead71a3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+      'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
+      'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
+      'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg',
+      'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
     ]
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * [canvas] Load cssa logo.
    */
-  onLoad: function (options) {
-    wx.showModal({
-      title: 'Hello',
-      content: 'Welcome to UMD CSSA WeApp Page 2!'
+  loadCanvasCssaLogo() {
+    // When canvas is not supported.
+    if (!wx.createCanvasContext) {
+      getApp().onError('Feature [canvas] not supported! Please update WeChat!')
+      return
+    }
+
+    wx.getSystemInfo({
+      success(res) {
+        console.log(res)
+
+        const screenWidth = res.screenWidth ?
+          res.screenWidth : res.windowWidth
+        const scaleFactor = screenWidth / 20
+        const cc = wx.createCanvasContext("cssa-logo")
+
+        cc.setStrokeStyle("#C00")
+        cc.setLineWidth(10)
+        cc.setLineCap("round")
+        cc.setLineJoin("round")
+
+        cc.moveTo(18 * scaleFactor, 2 * scaleFactor)
+        cc.lineTo(2 * scaleFactor, 2 * scaleFactor)
+        cc.lineTo(2 * scaleFactor, 14 * scaleFactor)
+        cc.lineTo(18 * scaleFactor, 14 * scaleFactor)
+        cc.moveTo(10 * scaleFactor, 2 * scaleFactor)
+        cc.lineTo(10 * scaleFactor, 6 * scaleFactor)
+        cc.lineTo(18 * scaleFactor, 6 * scaleFactor)
+        cc.lineTo(18 * scaleFactor, 18 * scaleFactor)
+        cc.moveTo(2 * scaleFactor, 18 * scaleFactor)
+        cc.lineTo(10 * scaleFactor, 18 * scaleFactor)
+        cc.lineTo(10 * scaleFactor, 10 * scaleFactor)
+        cc.moveTo(2 * scaleFactor, 10 * scaleFactor)
+        cc.lineTo(18 * scaleFactor, 10 * scaleFactor)
+        cc.stroke()
+        cc.draw()
+      }
     })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * Lifecycle, called on render finished.
    */
-  onReady: function () {
-
+  onReady() {
+    this.loadCanvasCssaLogo()
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * Called when an error callback was executed.
    */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onError(msg) {
+    getApp().onError(msg)
   }
 })
