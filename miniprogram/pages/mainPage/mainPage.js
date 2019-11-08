0459b8721819017935fe1c110cdc13f5e1ddda12
syKevinPeng <siyuan.kevin.peng@gmail.com>
2019-11-07 22:31:11 -0500

    Major Changes to Home Page

--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -1,75 +1,24 @@
-// miniprogram/pages/mainPage/mainPage.js
+//mainPage.js
 Page({
 
   /**
-   * Page data.
+   * Data in page
    */
   data: {
-    // [swiper] image slider
-    imgSliderUrls: [
-      'https://images.unsplash.com/photo-1558637845-c8b7ead71a3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
-      'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
-      'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
-      'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg',
-      'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
-    ]
   },
 
-  /**
-   * [canvas] Load cssa logo.
-   */
-  // loadCanvasCssaLogo() {
-    // When canvas is not supported.
-    // if (!wx.createCanvasContext) {
-    //   getApp().onError('Feature [canvas] not supported! Please update WeChat!')
-    //   return
-    // }
-
-    // wx.getSystemInfo({
-    //   success(res) {
-    //     console.log(res)
-
-    //     const screenWidth = res.screenWidth ?
-    //       res.screenWidth : res.windowWidth
-    //     const scaleFactor = screenWidth / 20
-    //     const cc = wx.createCanvasContext("cssa-logo")
-
-    //     cc.setStrokeStyle("#C00")
-    //     cc.setLineWidth(10)
-    //     cc.setLineCap("round")
-    //     cc.setLineJoin("round")
-
-    //     cc.rect(.5 * scaleFactor, .5 * scaleFactor, 19 * scaleFactor, 19 * scaleFactor)
-    //     cc.moveTo(18 * scaleFactor, 2 * scaleFactor)
-    //     cc.lineTo(2 * scaleFactor, 2 * scaleFactor)
-    //     cc.lineTo(2 * scaleFactor, 14 * scaleFactor)
-    //     cc.lineTo(18 * scaleFactor, 14 * scaleFactor)
-    //     cc.moveTo(10 * scaleFactor, 2 * scaleFactor)
-    //     cc.lineTo(10 * scaleFactor, 6 * scaleFactor)
-    //     cc.lineTo(18 * scaleFactor, 6 * scaleFactor)
-    //     cc.lineTo(18 * scaleFactor, 18 * scaleFactor)
-    //     cc.moveTo(2 * scaleFactor, 18 * scaleFactor)
-    //     cc.lineTo(10 * scaleFactor, 18 * scaleFactor)
-    //     cc.lineTo(10 * scaleFactor, 10 * scaleFactor)
-    //     cc.moveTo(2 * scaleFactor, 10 * scaleFactor)
-    //     cc.lineTo(18 * scaleFactor, 10 * scaleFactor)
-    //     cc.stroke()
-    //     cc.draw()
-    //   }
-    // })
-  // },
-
-  /**
-   * Lifecycle, called on render finished.
-   */
-  onReady() {
-    // this.loadCanvasCssaLogo()
-  },
-
-  /**
-   * Called when an error callback was executed.
-   */
-  onError(msg) {
-    getApp().onError(msg)
+  onLoad() {
+    wx.getSystemInfo({
+      success:res => {
+        this.setData({
+          winWidth : res.windowWidth,
+          winHeight : res.windowHeight,
+          /*ajusted img height and width are defined here. We preserve aspect ratio*/
+          imgHeight: res.windowHeight * 0.9, 
+          imgWidth: (res.windowHeight * 0.9) / 1350 *750, 
+          })
+        console.log("Img Height: " + this.setData.imgHeight + " Img Width: " + this.setData.imgWidth)
+      }
+    })
   }
 })
