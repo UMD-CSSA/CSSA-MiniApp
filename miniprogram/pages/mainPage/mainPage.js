078186b4a3716adf2e93ec71caeee6f775833a1f
Siyuan “Kevin” Peng <siyuan.kevin.peng@gmail.com>
2019-11-08 22:43:22 -0500

    update to  homepage

--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -5,33 +5,44 @@ Page({
    * Data in page
    */
   data: {
+    model:''
   },
 
   onLoad() {
     wx.getSystemInfo({
       success:res => {
         this.setData({
+          model:res.model,
           winWidth : res.windowWidth,
           winHeight : res.windowHeight,
-          /*ajusted img height and width are defined here. We preserve aspect ratio*/
-          imgHeight: res.windowHeight * 0.9, 
-          imgWidth: (res.windowHeight * 0.9) / 1350 *750, 
           })
-        console.log("Img Height: " + this.setData.imgHeight + " Img Width: " + this.setData.imgWidth)
+      },
+      fail:res =>{
+        this.setData({
+          model:NONE
+        })
+      },
+      complete: res =>{
+        model = this.setData.model;
+        if(model == "iPhone 6" || model == "iPhone 7" || model == "iPhone8"){
+          console.log("THis is a iphone 6 or 7 or 8")
+        }else{
+          this.setData({
+            imgHeight: res.windowHeight * 0.9,
+            imgWidth: (res.windowHeight * 0.9) / 1350 * 750,
+            btnHeight: 230 / 750 * (res.windowHeight),
+            btnRight: 80 / 375 * ((res.windowHeight * 0.9) / 1350 * 750),
+            btnMarginTop: 270 / 740 * (res.windowHeight)
+          })
+        }
+        console.log("curr width: " + res.windowWidth),
+        console.log("btnHeight: " + 380 / 375 * res.windowWidth)
       }
     })
   },
-  onReady: function () {
-    var my_carvas = wx.createCanvasContext('myCanvas', this) 
-    my_carvas.setStrokeStyle('red') 
-    my_carvas.rect(10, 10, 100, 30)
-    my_carvas.stroke()  
-    my_carvas.draw()  
-  },
-
-  naviToSetting() {
+  naviToSetting: function(event) {
     wx.navigateTo({
-      url: 'loginPage',
-    })
-  }
+      url:"pages/loginPage/loginPage"
+      })
+    }
 })
