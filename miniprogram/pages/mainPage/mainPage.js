4591d128c811d3fd90dc116abb4e80e5b3026c27
syKevinPeng <siyuan.kevin.peng@gmail.com>
2019-11-09 01:04:10 -0500

    3 buttons in home page working;
    Signed-off-by: syKevinPeng <siyuan.kevin.peng@gmail.com>
    Co-Authored-By: Jerry <jerryc05@qq.com>

--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -13,11 +13,8 @@ Page({
       getApp().onError("wx.getSystemInfoSync() return nothing")
     }
 
-    this.setData({
-      winWidth: res.windowWidth,
-      winHeight: res.windowHeight,
-    })
-
+    console.warn(res.windowWidth)
+    console.warn(res.windowHeight)
 
     const imgHeight = 1350
     const btnHeight = 255
@@ -31,19 +28,42 @@ Page({
     const _btnWidth = _btnHeight * btnAspectRatio
 
     this.setData({
+      winWidth: res.windowWidth,
+      winHeight: res.windowHeight,
       imgHeight: _imgHeight,
       imgWidth: _imgWidth,
       btnHeight: _btnHeight,
       btnWidth: _btnWidth,
-      btnMarginRight: _imgWidth * 90 / 750,
-      btnMarginTop1: _imgHeight * 270 / 1350,
-      btnMarginTop2: _imgHeight * 270 / 1350 + 280,
-      btnMarginTop3: _imgHeight * 270 / 1350 + 280 * 2,
+      btnMarginRight: _imgWidth * 3 / 25, // 90 / 750,
+      btnMarginTop1: _imgHeight / 5, // * 270 / 1350,
+      btnMarginTop2: _imgHeight / 5 + _btnHeight * 11 / 10,
+      btnMarginTop3: _imgHeight / 5 + _btnHeight * 11 / 5,
+      btnMarginTop4: _imgHeight / 5 + _btnHeight * 11 / 10 * 3,
     })
   },
 
-  onBgImgClicked(event) {
-    console.log(123)
-    console.info(event)
+  onClicked(event) {
+    if (!event.detail)
+      getApp().onError("evemt.detail not found!")
+
+    // console.info(event.detail)
+    const x = event.detail.x
+    const y = event.detail.y
+
+    if (this.data.winWidth - x < this.data.btnMarginRight + this.data.btnWidth
+      && this.data.winWidth - x > this.data.btnMarginRight) {
+
+      if (y < this.data.btnMarginTop2 && y > this.data.btnMarginTop1) {
+        console.warn("Button 1!")
+      }
+
+      else if (y < this.data.btnMarginTop3 && y > this.data.btnMarginTop2) {
+        console.warn("Button 2!")
+      }
+
+      else if (y < this.data.btnMarginTop4 && y > this.data.btnMarginTop3) {
+        console.warn("Button 3!")
+      }
+    }
   }
 })
